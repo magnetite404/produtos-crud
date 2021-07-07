6e67105646da2572033ea902a1aac1e6b09e4eea
Rai Vargas <raidavid.159@hotmail.com>
2021-07-06 23:11:47 -0300

    Componente de alterar produto finalizado

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { ProductRead2Component } from './components/product/product-read2/produc
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 
 registerLocaleData(localePt);
 
@@ -42,7 +43,8 @@ registerLocaleData(localePt);
     ProductCrudComponent,
     ProductCreateComponent,
     ProductReadComponent,
-    ProductRead2Component
+    ProductRead2Component,
+    ProductUpdateComponent
   ],
   imports: [
     BrowserModule,
